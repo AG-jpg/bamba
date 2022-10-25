adba87c9542560cc17c50402de09778b1f21e8e5
Aldo Guzman <75010573+AG-jpg@users.noreply.github.com>
2022-10-25 13:18:58 +0300

    Coin Grab Funciton

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,14 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
     public GameObject vidasUI;
+    public PlayerController player;
+    public Text textCoin;
+    public int coins;
 
     private void Awake() 
     {
-        instance = this;
+        if(instance == null)
+        {
+            instance = this;
+        } else
+        {
+            Destroy(this.gameObject);
+        }
+        
+    }
+
+    public void UpdateCoinCounter()
+    {
+        coins++;
+        textCoin.text = coins.ToString();
     }
 }
