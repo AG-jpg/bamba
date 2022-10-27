43312825562c2749f5c62d4fa6d1870f0d24d67e
Aldo Guzman <75010573+AG-jpg@users.noreply.github.com>
2022-10-27 15:59:24 +0300

    Fall Audio and Time Fix

--- Assets/Scripts/Fall.cs
+++ Assets/Scripts/Fall.cs
@@ -5,13 +5,28 @@ using UnityEngine;
 public class Fall : MonoBehaviour
 {
     public GameObject checkpoint;
+    [Header ("Audio")]
+    private AudioSource audioSource;
+    [SerializeField] private AudioClip fall;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
 
     private void OnTriggerEnter2D(Collider2D collision) 
     {
         if(collision.CompareTag("Player"))
         {
+            audioSource.PlayOneShot(fall);
+            WaitTime();
             collision.transform.position = checkpoint.transform.position;
             collision.GetComponent<PlayerController>().RecibirDano();
         }
+    }
+
+    private IEnumerator WaitTime()
+    {
+        yield return new WaitForSeconds(1);
     }
 }
