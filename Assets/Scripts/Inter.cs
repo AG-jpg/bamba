0f14c7e0fe75f9f80d4ef70e4bf6690821719011
Aldo Guzman <75010573+AG-jpg@users.noreply.github.com>
2022-11-12 15:03:57 +0200

    Level 2 Scenario

--- Assets/Scripts/Inter.cs
+++ Assets/Scripts/Inter.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class ToLevel2 : MonoBehaviour
+public class Inter : MonoBehaviour
 {
     public void OnTriggerEnter2D(Collider2D other)
     {
         if(other.tag == "Player")
         {
-            SceneManager.LoadScene(4);
+            SceneManager.LoadScene(3);
         }
     }
 }
