a6fa504d572b41fd7521a4b3df0aafba24e4c65d
Aldo Guzman <75010573+AG-jpg@users.noreply.github.com>
2022-12-11 23:16:50 +0200

    Life Up Added in UI

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -7,6 +7,11 @@ public class Item : MonoBehaviour
     public PlayerController player;
     public GameObject heart;
 
+    //Positions
+    private Vector2 positionThree;
+    private Vector2 positionTwo;
+    private Vector2 positionOne;
+
     private void OnTriggerEnter2D(Collider2D collision) 
     {
         if(collision.CompareTag("Player"))
@@ -22,10 +27,33 @@ public class Item : MonoBehaviour
             GameManager.instance.UpdateCoinCounter();
         }else if(gameObject.CompareTag("Life"))
         {
+            PositionHeart();
             player.AddLife();
-            heart.SetActive(true);
         }
         
         Destroy(gameObject);
+    }
+
+    public void PositionHeart()
+    {
+        positionThree = new Vector2(-760f, 372.4404f);
+        positionTwo = new Vector2(-840f, 372.4404f);
+        positionOne = new Vector2(-920f, 372.4404f);
+        
+        if(player.vidas == 3)
+        {
+            heart.SetActive(true);
+            heart.transform.localPosition = positionThree;
+        }
+        else if(player.vidas == 2)
+        {
+            heart.SetActive(true);
+            heart.transform.localPosition = positionTwo;
+        }
+        else if(player.vidas == 1)
+        {
+            heart.SetActive(true);
+            heart.transform.localPosition = positionOne;
+        }
     }
 }
