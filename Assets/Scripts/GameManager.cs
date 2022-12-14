530995e97e0b8ffae37df117b249e76c0cf0d11f
Aldo Guzman <75010573+AG-jpg@users.noreply.github.com>
2022-12-14 23:31:51 +0200

    Add Life by Coins

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,11 +9,16 @@ public class GameManager : MonoBehaviour
     public static GameManager instance;
     public GameObject vidasUI;
     public PlayerController player;
+    public Item item;
     public GameObject pausePanel;
 
     [Header("Coins Panel")]
     public int coins;
     public Text textCoin;
+
+    [Header("Audio")]
+    private AudioSource audioSource;
+    [SerializeField] private AudioClip chimes;
 
      private void Awake() 
     {
@@ -25,12 +30,31 @@ public class GameManager : MonoBehaviour
         {
             Destroy(this.gameObject);
         }
+
+        audioSource = GetComponent<AudioSource>();
     }
 
     public void UpdateCoinCounter()
     {
         coins++;
         textCoin.text = coins.ToString();
+    }
+
+    public void CoinLife()
+    {
+        if(coins == 100)
+        {
+            audioSource.PlayOneShot(chimes);
+            item.PositionHeart();
+            player.AddLife();
+            coins = 0;
+        }
+    }
+
+    /// Update is called every frame, if the MonoBehaviour is enabled.
+    private void Update()
+    {
+        CoinLife();
     }
 
     public void FinishPause()
