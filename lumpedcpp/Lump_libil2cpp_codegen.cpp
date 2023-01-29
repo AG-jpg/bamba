2c603b67fc10b93d9fb4e5bbc00463d2411246ec
Aldo Guzman <75010573+AG-jpg@users.noreply.github.com>
2023-01-29 16:27:12 +0200

    Library Update

---
+++ lumpedcpp/Lump_libil2cpp_codegen.cpp
@@ -0,0 +1,4 @@
+#include "il2cpp-config.h"
+#include "/Volumes/RESPALDO/Code/Unity/2020.3.25f1/Unity.app/Contents/il2cpp/libil2cpp/codegen/il2cpp-codegen-common.cpp"
+#include "/Volumes/RESPALDO/Code/Unity/2020.3.25f1/Unity.app/Contents/il2cpp/libil2cpp/codegen/il2cpp-codegen-il2cpp.cpp"
+#include "/Volumes/RESPALDO/Code/Unity/2020.3.25f1/Unity.app/Contents/il2cpp/libil2cpp/codegen/il2cpp-codegen-tiny.cpp"
---
+++ lumpedcpp/Lump_libil2cpp_gc.cpp
@@ -0,0 +1,7 @@
+#include "il2cpp-config.h"
+#include "/Volumes/RESPALDO/Code/Unity/2020.3.25f1/Unity.app/Contents/il2cpp/libil2cpp/gc/BoehmGC.cpp"
+#include "/Volumes/RESPALDO/Code/Unity/2020.3.25f1/Unity.app/Contents/il2cpp/libil2cpp/gc/GCHandle.cpp"
+#include "/Volumes/RESPALDO/Code/Unity/2020.3.25f1/Unity.app/Contents/il2cpp/libil2cpp/gc/GarbageCollector.cpp"
+#include "/Volumes/RESPALDO/Code/Unity/2020.3.25f1/Unity.app/Contents/il2cpp/libil2cpp/gc/NullGC.cpp"
+#include "/Volumes/RESPALDO/Code/Unity/2020.3.25f1/Unity.app/Contents/il2cpp/libil2cpp/gc/WriteBarrier.cpp"
+#include "/Volumes/RESPALDO/Code/Unity/2020.3.25f1/Unity.app/Contents/il2cpp/libil2cpp/gc/WriteBarrierValidation.cpp"
