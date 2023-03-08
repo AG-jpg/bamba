4745f424d023593793cd95081bfac6f409bf0741
Aldo Guzman <75010573+AG-jpg@users.noreply.github.com>
2023-03-08 18:15:25 +0200

    Ballon Drops Life Speed Fixed

--- Assets/Scripts/Drop.cs
+++ Assets/Scripts/Drop.cs
@@ -18,7 +18,7 @@ public class Drop : MonoBehaviour
         rend = Life.GetComponent<SpriteRenderer>();
     }
 
-        private void OnTriggerEnter2D(Collider2D collision)
+        private void OnTriggerExit2D(Collider2D collision)
     {
         if(collision.CompareTag("Life"))
         {
