97b71ec4003b19286a261d3ce318971731fadf22
Aldo Guzman <75010573+AG-jpg@users.noreply.github.com>
2023-05-30 21:29:13 +0300

    Cambiar Vista Added to Boss

--- Assets/Scripts/Boss/Boss.cs
+++ Assets/Scripts/Boss/Boss.cs
@@ -95,4 +95,21 @@ public class Boss : MonoBehaviour
         }
     }
 
+    public void Shake()
+    {
+        impact.SetMakesDamage(true);
+        
+    }
+
+    private void CambiarVista(float direccionX)
+        {
+            if(direccionX < 0 && transform.localScale.x > 0)
+            {
+                transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, transform.localScale.z);
+            }else if(direccionX > 0 && transform.localScale.x < 0)
+            {
+                transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+            }
+        }
+
 }
