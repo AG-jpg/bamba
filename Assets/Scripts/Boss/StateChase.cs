1eba2d3925ce81c8aea404ac3ce3269c4e58e918
Aldo Guzman <75010573+AG-jpg@users.noreply.github.com>
2023-06-02 17:29:19 +0300

    State Chase Done

--- Assets/Scripts/Boss/StateChase.cs
+++ Assets/Scripts/Boss/StateChase.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class StateChase : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Boss boss;
+
+   private void Awake()
+   {
+    boss = GetComponent<Boss>();
+   }
+
+       private void CambiarVista(float direccionX)
     {
-        
+        if (direccionX < 0 && transform.localScale.x > 0)
+        {
+            transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, transform.localScale.z);
+        }
+        else if (direccionX > 0 && transform.localScale.x < 0)
+        {
+            transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if(boss.distance <= boss.attackArea)
+        {
+            boss.ActivateState(boss.attackState);
+        }
+        else
+        {
+            if(!boss.isAttacking && !boss.isKO)
+            {
+                Movement();
+            }
+        }
+    }
+
+    private void Movement()
+    {
+        if(boss.distance <= boss.detectionArea)
+        {
+            Vector3 direccion = (transform.position - boss.player.transform.position).normalized;
+            CambiarVista(-direccion.x);
+            boss.rb.velocity = new Vector3(-direccion.x * boss.speedMovement, boss.rb.velocity.y);
+        }
     }
 }
