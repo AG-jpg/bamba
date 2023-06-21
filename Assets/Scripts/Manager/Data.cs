2b0200ea8a35f2d9604ee4b75cf3cece4a5b6a93
Aldo Guzman <75010573+AG-jpg@users.noreply.github.com>
2023-06-21 15:41:31 +0300

    Data Script

---
+++ Assets/Scripts/Manager/Data.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Data
+{
+    //Inventario Data
+    public static int notes;
+
+    // Stats Data
+    public static int level;
+    public static int vida;
+
+}
