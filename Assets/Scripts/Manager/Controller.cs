d7709d71e8c047cc173df0b6ad0c50c9db41475e
Aldo Guzman <75010573+AG-jpg@users.noreply.github.com>
2023-06-23 12:36:11 +0300

    Save Update on Data and Controller

--- Assets/Scripts/Manager/Controller.cs
+++ Assets/Scripts/Manager/Controller.cs
@@ -7,12 +7,7 @@ public class Controller : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //Valores iniciales
-        Data.notes = 0;
-
-        Data.vida = 3;
-        Data.velocidad = 5;
-        Data.salto = 10;
+       
     }
 
     // Update is called once per frame
