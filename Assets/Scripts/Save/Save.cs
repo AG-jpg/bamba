1b458f83a99433aacdfbff7facb6f1c50919d561
Aldo Guzman <75010573+AG-jpg@users.noreply.github.com>
2023-06-23 11:47:19 +0300

    Save Scripts

---
+++ Assets/Scripts/Save/Save.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+
+public class Save
+{
+    //Inventario Data
+    public int notes;
+
+    // Stats Data
+    public int level;
+    public int vida;
+    public int velocidad;
+    public int salto;
+
+
+}
