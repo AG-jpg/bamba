d6c4dfa6b14867fdda48126f67f5c82fc009c947
Aldo Guzman <75010573+AG-jpg@users.noreply.github.com>
2023-06-23 11:55:22 +0300

    Save Control Script Update

--- Assets/Scripts/Save/SaveControl.cs
+++ Assets/Scripts/Save/SaveControl.cs
@@ -1,17 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class SaveControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    
+    private void Start()
     {
-        
+        if(SceneManager.GetActiveScene().Equals(SceneManager.GetSceneByBuildIndex(0)))
+        {
+            LoadGame();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private Save CreateSavedGameObject()
+    {
+        Save info = new Save();
+
+        //Inventario Data
+        info.notes = Data.notes;
+
+        // Stats Data
+        info.level = Data.level;
+        info.vida = Data.vida;
+        info.velocidad = Data.velocidad;
+        info.salto = Data.salto;
+
+        return info;
+    }
+
+    public void SaveGame()
+    {
+
+    }
+
+    public void LoadGame()
     {
         
     }
